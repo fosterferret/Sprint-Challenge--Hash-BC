3e1bc29a4177ad7925ac3cb5999a2423e0173e89
Francis Bulus <notchera@gmail.com>
2020-03-13 10:06:33 +0100

    used random module for proof

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -28,7 +28,7 @@ def proof_of_work(last_proof):
     last_hash = hashlib.sha256(f'{last_proof}'.encode()).hexdigest()
 
     while not valid_proof(last_hash, proof):
-        proof += 1
+        proof = random.getrandbits(32)
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
